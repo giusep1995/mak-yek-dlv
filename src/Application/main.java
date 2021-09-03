546995863e1c94d522615079afc4b42b62db3b93
Francesco Rota <francescorota99@gmail.com>
2021-09-03 20:07:40 +0200

    Application Package

--- src/Application/main.java
+++ src/Application/main.java
@@ -1,7 +1,10 @@
 package Application;
 
-public class main {
+public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		// TODO Auto-generated method stub
+
 	}
+
 }
